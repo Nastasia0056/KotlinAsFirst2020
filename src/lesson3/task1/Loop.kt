81feedffaa33c74bf897e534ea5ec725438b0851
Apalko Nastya <apalkolu@gmail.com>
2020-12-04 14:52:36 +0300

    Решена задача fib

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -80,7 +80,20 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+fun fib(n: Int): Int {
+    if (n in 1..2) return 1
+    var a = 1
+    var b = 1
+    var count = 2
+    while (count < n) {
+        count++
+        val c = a + b
+        a = b
+        b = c
+    }
+    return b
+}
+
 
 /**
  * Простая (2 балла)
