45ddb5b147c5d692941ae405fec0669f2330463c
Apalko Nastya <apalkolu@gmail.com>
2021-03-01 01:07:53 +0300

    Решен Complex (были проблемы с тестами)

--- src/lesson11/task1/Complex.kt
+++ src/lesson11/task1/Complex.kt
@@ -2,6 +2,9 @@
 
 package lesson11.task1
 
+import kotlin.math.abs
+import kotlin.math.pow
+
 /**
  * Класс "комплексное число".
  *
@@ -16,45 +19,101 @@ class Complex(val re: Double, val im: Double) {
     /**
      * Конструктор из вещественного числа
      */
-    constructor(x: Double) : this(TODO(), TODO())
+    constructor(x: Double) : this(x.toInt().toDouble(), x - x.toInt())
 
     /**
      * Конструктор из строки вида x+yi
      */
-    constructor(s: String) : this(TODO(), TODO())
+    constructor(s: String) : this(getRe(s), getIm(s))
+
+    companion object {
+        private fun getRe(s: String): Double = s.substring(0, getIndex(s)).toDouble()
+
+        private fun getIm(s: String): Double {
+            val i = getIndex(s);
+            val im = s.substring(i, s.length - 1).toDouble()
+            return if (s.toCharArray()[i] == '+') im
+            else abs(im) * -1
+        }
+
+        private fun getIndex(s: String): Int {
+            val plus = s.indexOf('+')
+            return if (plus != -1) plus
+            else s.indexOf('-')
+        }
+    }
 
     /**
      * Сложение.
      */
-    operator fun plus(other: Complex): Complex = TODO()
+    operator fun plus(other: Complex): Complex {
+        val r = re + other.re
+        val i = im + other.im
+        return Complex(r, i)
+    }
 
     /**
      * Смена знака (у обеих частей числа)
      */
-    operator fun unaryMinus(): Complex = TODO()
+    operator fun unaryMinus(): Complex = Complex(-re, -im)
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Complex): Complex = TODO()
+    operator fun minus(other: Complex): Complex {
+        val r = re - other.re
+        val i = im - other.im
+        return Complex(r, i)
+    }
 
     /**
      * Умножение
      */
-    operator fun times(other: Complex): Complex = TODO()
+    operator fun times(other: Complex): Complex {
+        val r = re * other.re - im * other.im
+        val i = re * other.im + im * other.re
+        return Complex(r, i)
+    }
+
 
     /**
      * Деление
      */
-    operator fun div(other: Complex): Complex = TODO()
+
+    operator fun div(other: Complex): Complex {
+        val bot = (other.re.pow(2) + other.im.pow(2))
+        val r = ((re * other.re) + (im * other.im)) / bot
+        val i = ((im * other.re) - (re * other.im)) / bot
+        return Complex(r, i)
+    }
+
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (other == null) return false
+        if (other !is Complex) return false
+        val comp = other
+        return re == comp.re && im == comp.im
+    }
+
 
     /**
      * Преобразование в строку
      */
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        if (im == 0.0) return "$re"
+        if (re == 0.0) return "${im}i"
+        if (im < 0.0) return "$re-${-im}i"
+        return "$re+${im}i"
+    }
+
 }
+
+
+
+
+
+
+
