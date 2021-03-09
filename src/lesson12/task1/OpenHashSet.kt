e3564dbb7b1f143f2b00df832eadd68b3aa17523
Apalko Nastya <apalkolu@gmail.com>
2021-03-09 15:39:14 +0300

    Выполнен OpenHashSet

--- src/lesson12/task1/OpenHashSet.kt
+++ src/lesson12/task1/OpenHashSet.kt
@@ -21,32 +21,97 @@ class OpenHashSet<T>(val capacity: Int) {
      * Массив для хранения элементов хеш-таблицы
      */
     internal val elements = Array<Any?>(capacity) { null }
+    var count = 0
 
     /**
      * Число элементов в хеш-таблице
      */
-    val size: Int get() = TODO()
+    val size: Int get() = this.count
+
+    /**
+     * Максимальное число элементов в хеш-таблице
+     */
+    val maxSize: Int get() = this.capacity
 
     /**
      * Признак пустоты
      */
-    fun isEmpty(): Boolean = TODO()
+    fun isEmpty(): Boolean = this.count <= 0
+
+    private fun Any?.hashCode(): Int = this?.hashCode() ?: 0
 
     /**
      * Добавление элемента.
      * Вернуть true, если элемент был успешно добавлен,
      * или false, если такой элемент уже был в таблице, или превышена вместимость таблицы.
      */
-    fun add(element: T): Boolean = TODO()
+    fun add(element: T): Boolean {
+        if (count != capacity) {
+            if (this.contains(element)) {
+                return false
+            } else {
+                var i = 0
+                while (this.elements[(element.hashCode() + i) % capacity] != null) {
+                    i++
+                }
+                this.elements[(element.hashCode() + i) % capacity] = element
+                count++
+                return true
+            }
+        }
+        return false
+    }
 
     /**
      * Проверка, входит ли заданный элемент в хеш-таблицу
      */
-    operator fun contains(element: T): Boolean = TODO()
+    operator fun contains(element: T): Boolean {
+        var i = 0
+        while (this.elements[(element.hashCode() + i) % capacity] != null) {
+            if (this.elements[(element.hashCode() + i) % capacity] == element) {
+                return true
+            }
+            i++
+            if (i == this.capacity) {
+                break
+            }
+        }
+        return false
+    }
 
     /**
      * Таблицы равны, если в них одинаковое количество элементов,
      * и любой элемент из второй таблицы входит также и в первую
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (other?.javaClass != javaClass) return false
+        other as OpenHashSet<T>
+        if (other.size == this.size) {
+            for (i in 0..(this.size - 1)) {
+                if (this.elements[i] != null) {
+                    var index = (this.elements[i].hashCode() % this.capacity)
+                    var j = 0
+                    var count = 0
+                    var flag = false
+                    while (other.elements[index + j] != null) {
+                        if (other.elements[index + j] == this.elements[i]) {
+                            flag = true
+                        }
+                        j++
+                        count++
+                        if((index + j)==other.capacity){
+                            index = 0
+                            j=0
+                        }
+                        if(count == other.capacity)
+                            break
+                    }
+                    if (flag == false)
+                        return false
+                }
+            }
+            return true
+        }
+        return false
+    }
 }
